6933f5147dac0439c2d2af3588b44b682f468b59
Hardik Modha <hardik.modha27@gmail.com>
2024-04-24 10:07:25 +0530

    Initial Commit

---
+++ DocumentProgressTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DocumentProgressTask
+//
+//  Created by Hardik Modha on 24/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DocumentProgressTask/DocumentProgressTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentProgressTaskApp.swift
+//  DocumentProgressTask
+//
+//  Created by Hardik Modha on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DocumentProgressTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
